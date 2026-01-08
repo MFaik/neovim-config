e15439bcce1893a950c9290e11be60210ec5d6f6
mfaik <m.faikekclr@gmail.com>
2026-01-09 00:39:55 +0900

    I have no idea

--- lua/faik/plugins/harpoon.lua
+++ lua/faik/plugins/harpoon.lua
@@ -6,9 +6,9 @@ return {
 	},
 	config = true,
 	keys = {
-		{ "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-		{ "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-		{ "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
+		{ "<leader>hh", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
+		{ "<leader>hd", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
+		{ "<leader>hs", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
 		{ "<leader>ha", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
 	},
 }
